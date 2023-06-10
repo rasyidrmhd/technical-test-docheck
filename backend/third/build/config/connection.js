683bbc09a4ec2e493025ad5bc2cc8b86c431fb8c
rasyidrmhd <rasyidrmuhammad@gmail.com>
2023-06-10 12:29:16 +0700

    ignore build folder

--- backend/third/build/config/connection.js
+++ backend/third/build/config/connection.js
@@ -6,11 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const pool = new pg_1.Pool({
-    user: process.env.POSTGRES_USER,
-    host: "localhost",
-    database: "simple_todolist",
-    password: process.env.POSTGRES_PASSWORD,
-    port: 5432,
-});
+let pool;
+if (process.env.NODE_ENV === "production") {
+    pool = new pg_1.Pool({
+        connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    });
+}
+else {
+    pool = new pg_1.Pool({
+        user: process.env.POSTGRES_USER,
+        host: "localhost",
+        database: "simple_todolist",
+        password: process.env.POSTGRES_PASSWORD,
+        port: 5432,
+    });
+}
 exports.default = pool;
