b4bca8fc3442f13c82ee5372ee14bed198ec12f4
rasyidrmhd <rasyidrmuhammad@gmail.com>
2023-06-11 10:44:36 +0700

    implement react-redux provider and testing redux state

--- frontend/third/src/App.tsx
+++ frontend/third/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { useAppSelector, useAppDispatch } from "./store";
+import { addTask } from "./store/features/taskSlice";
+import "./App.css";
 
 function App() {
+  const tasks = useAppSelector((state) => state.task.tasks);
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    console.log(tasks, ">>>>task");
+  }, [tasks]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +19,18 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+
+      <button
+        onClick={() => {
+          dispatch(addTask({ name: "belajar gitar" }));
+        }}
+      >
+        add
+      </button>
     </div>
   );
 }
